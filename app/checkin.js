33452e744c76f63c59db420790dad5e1fe3f1fd6
Andrew Davidson <andrew.davidson@synergy-learning.com>
2016-04-07 16:05:55 +0100

    Synergy formatting updates

--- app/checkin.js
+++ app/checkin.js
@@ -1,7 +1,7 @@
 "use strict";
 
 if (process.env.NODE_ENV !== "production") {
-  require("dotenv").load();
+    require("dotenv").load();
 }
 
 var moment = require("moment");
@@ -17,67 +17,165 @@ var conjunct = require("./lib/conjunct");
 var slack = require("./lib/slack");
 
 var options = {
-  startDate: moment(),
-  endDate: moment()
+    startDate: moment(),
+    endDate: moment()
 };
 
 // skip if weekend
 if (process.env.SKIP_IF_WEEKEND && (moment().day() === 6 || moment().day() === 0)) {
-  console.log("It's weekend, skipping...");
-  process.exit(); // eslint-disable-line no-process-exit
+    console.log("It's weekend, skipping...");
+    process.exit(); // eslint-disable-line no-process-exit
 }
 
 Promise.all([
-  forecast.people(),
-  forecast.projects(),
-  forecast.clients(),
-  forecast.assignments(options)
+    forecast.people(),
+    forecast.projects(),
+    forecast.clients(),
+    forecast.assignments(options)
 ]).then(data => {
-  let people = data[0];
-  let projects = lookup(data[1]);
-  let clients = lookup(data[2]);
-  let assignments = data[3];
+    let people = data[0];
+    let projects = lookup(data[1]);
+    let clients = lookup(data[2]);
+    let assignments = data[3];
 
-  // exclude persons
-  people = peopleFilter.exclude(people, process.env.PEOPLE_EXCLUDE_FILTER);
+    // exclude persons
+    people = peopleFilter.exclude(people, process.env.PEOPLE_EXCLUDE_FILTER);
 
-  // sort persons alphabetically
-  people.sort((a, b) => a.first_name.localeCompare(b.first_name));
+    // sort persons alphabetically
+    people.sort((a, b) => a.first_name.localeCompare(b.first_name));
 
-  let msg = [];
+    let femsg = [];
+    let devmsg = [];
+    let desmsg = [];
+    let testmsg = [];
 
-  people.forEach(p => {
-    // get person activity for current day
-    let personActivity = activity.get(p, assignments);
-    if (personActivity.length === 0) {
-      // no entry for person
-      msg.push(`${personName(p)} -  has no entry for today.`);
-    } else if (personActivity.length === 1 && personActivity[0].project_id === parseInt(process.env.PROJECT_ID_TIME_OFF)) {
-      // person got time off and does nothing else
-      let endDate = personTimeOff(personActivity);
-      msg.push(`${personName(p)}  -  is off and will be back ${endDate.format("YYYY-MM-DD")}.`);
-    } else {
-      // normal assignments (but ignore partial day time off)
-      let activities = personActivities(personActivity, projects, clients);
-      msg.push(`${personName(p)}  -   will be working on ${conjunct(activities)}.`);
-    }
-  });
+    people.forEach(p => {
+        if (p.archvied == true) {
+            return;
+        }
+        // get person activity for current day
+        let personActivity = activity.get(p, assignments);
+        if (personActivity.length === 0) {
+            // no entry for person
+            let text = `${personName(p)}    -  No entry for today.`;
 
-  // send as Slack msg
-  slack.send({
-    attachments: [
-      {
-         "fallback": `${options.startDate.format("dddd YYYY-MM-DD")} according to Forecast...`,
-         "pretext": `${options.startDate.format("dddd YYYY-MM-DD")} :sunrise: according to <${process.env.FORECAST_TEAM_URL}|Forecast>...`,
-         "color": "good",
-         "mrkdwn_in": ["pretext", "text", "fields"],
-         "fields": [
-            {
-               "value": msg.join("\n"),
-               "short": false
+            for(var i = 0; i < p.teams.length; i++) {
+                if(p.teams[i] == 'Developer') {
+                    devmsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Tester') {
+                    testmsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Front-end') {
+                    femsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Design') {
+                    desmsg.push(text);
+                    return;
+                }
             }
-         ]
-      }
-     ]
-  });
+        } else if (personActivity.length === 1 && personActivity[0].project_id === parseInt(process.env.PROJECT_ID_TIME_OFF)) {
+            // person got time off and does nothing else
+            let endDate = personTimeOff(personActivity);
+            let text = `${personName(p)}    - Off today and will be back ${endDate.format("YYYY-MM-DD")}.`
+            for(var i = 0; i < p.teams.length; i++) {
+                if(p.teams[i] == 'Developer') {
+                    devmsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Tester') {
+                    testmsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Front-end') {
+                    femsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Design') {
+                    desmsg.push(text);
+                    return;
+                }
+            }
+        } else {
+            // normal assignments (but ignore partial day time off)
+            let activities = personActivities(personActivity, projects, clients);
+            let text = `${personName(p)}    - Working on ${conjunct(activities)}.`;
+            for(var i = 0; i < p.teams.length; i++) {
+                if(p.teams[i] == 'Developer') {
+                    devmsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Tester') {
+                    testmsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Front-end') {
+                    femsg.push(text);
+                    return;
+                }
+                if(p.teams[i] == 'Design') {
+                    desmsg.push(text);
+                    return;
+                }
+            }
+        }
+    });
+
+    // send as Slack msg
+    slack.send({
+        attachments: [
+            {
+                "fallback": `Synergy Studio Thursday 2016-04-07 according to Forecast`,
+                "pretext": `Synergy Studio ${options.startDate.format("dddd YYYY-MM-DD")} according to <${process.env.FORECAST_TEAM_URL}|Forecast>...`,
+                "mrkdwn_in": ["pretext"],
+            },
+            {
+                "pretext": `Design`,
+                "color": "#2ecc71",
+                "mrkdwn_in": ["pretext", "text", "fields"],
+                "fields": [
+                    {
+                        "value": desmsg.join("\n"),
+                        "short": false
+                    }
+                ]
+            },
+            {
+                "pretext": `Front-End`,
+                "color": "#4aa3df",
+                "mrkdwn_in": ["pretext", "text", "fields"],
+                "fields": [
+                    {
+                        "value": femsg.join("\n"),
+                        "short": false
+                    }
+                ]
+            },
+            {
+                "pretext": `Developer`,
+                "color": "#9b59b6",
+                "mrkdwn_in": ["pretext", "text", "fields"],
+                "fields": [
+                    {
+                        "value": devmsg.join("\n"),
+                        "short": false
+                    }
+                ]
+            },
+            {
+                "pretext": `Tester`,
+                "color": "#34495e",
+                "mrkdwn_in": ["pretext", "text", "fields"],
+                "fields": [
+                    {
+                        "value": testmsg.join("\n"),
+                        "short": false
+                    }
+                ]
+            }
+        ]
+    });
 }).catch(e => console.error(e));
